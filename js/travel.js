95269f66879b56d255a342920f259fec6775fbb8
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2015-11-29 14:49:34 -0500

    playing with search button on modal to hide and unhide search results

--- js/travel.js
+++ js/travel.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+
 $(function() {
   $( "#dateIn" ).datepicker();
 });
@@ -14,3 +16,28 @@ $(function() {
   $( "#modalDateOut" ).datepicker();
 });
 
+
+$("#findHotel").click(nySearch);
+function nySearch () {
+  document.getElementById("nyResults").style.display = "none";
+}
+
+
+
+// if the html matches any city then change css display to block
+
+    // $("findHotel").click(function() {
+    //     $(".chicagoHotels").h();
+    //     $(".dcHotels").hide();
+    //     $(".losAngelesHotels").hide();
+    //     $(".miamiHotels").hide();
+    //     $(".nyHotels").hide();
+    //     if ($(".chicagoHotels").css("display") === "none") {
+    //         $("").css({
+    //             'background-image' : 'url("public/images/expand-lg.png")'
+    //         });
+    //     }
+    //     else {} 
+    // });
+
+});
