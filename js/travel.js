ef7040a65a82fc85ccd852c00939d94662d952de
CB <cbmoate@gmail.com>
2015-11-30 17:12:41 -0500

    blocked out the side bar functions.  IDK if we have time to give them usable script

--- js/travel.js
+++ js/travel.js
@@ -1,17 +1,43 @@
 $(document).ready(function() {
 
-$("body").on('click', "#findHotel", function()
-{
-  citySearch($("#citySelector").val().trim());
-});
+  $("body").on('click', "#findHotel", function()
+  {
+    citySearch($("#citySelector").val().trim());
+  });
 
-function citySearch (citySelector) 
-{
-  if (citySelector === "New York")
+  function citySearch (citySelector) 
+  {
+    if (citySelector === "New York")
       $('#nyResults').show();
-  else if (citySelector === "Chicago")
+    else if (citySelector === "Chicago")
       $('#chicagoResults').show();
-}
+    else if (citySelector === "Miami")
+      $('#miamiResults').show();
+    else if (citySelector === "Washington DC")
+      $('#dcResults').show();
+    else if (citySelector === "Los Angeles")
+      $('#laResults').show();
+  }
+
+  function wifi ()
+  {
+
+  }
+
+  function cost ()
+  {
+
+  }
+
+  function pool ()
+  {
+
+  }
+
+  function restaurant ()
+  {
+    
+  }
 
   $(function() {
     $("#dateIn").datepicker();
