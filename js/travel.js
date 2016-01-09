00afa71348493c75f474b80ada619f0c912d9856
jmona789 <jmona789@gmail.com>
2016-01-08 23:34:55 -0500

    more formatting changes for JSLint

--- js/travel.js
+++ js/travel.js
@@ -1,46 +1,39 @@
-"use strict"
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 $(document).ready(function () {
 
-  $("body").on('click', "#findHotel", function ()
-  {
-    citySearch($("#citySelector").val().trim());
-  });
-
-  function citySearch (citySelector)
-  {
-    if (citySelector === "New York"){
+  function citySearch(citySelector) {
+    if (citySelector === "New York") {
       $('#nyResults').show();
-    }else if (citySelector === "Chicago"){
+    } else if (citySelector === "Chicago") {
       $('#chicagoResults').show();
-    }else if (citySelector === "Miami"){
+    } else if (citySelector === "Miami") {
       $('#miamiResults').show();
-    }else if (citySelector === "Washington DC"){
+    } else if (citySelector === "Washington DC") {
       $('#dcResults').show();
-    }else if (citySelector === "Los Angeles"){
+    } else if (citySelector === "Los Angeles") {
       $('#losAngelesResults').show();
     }
   }
 
-  function wifi ()
-  {
+  $("body").on('click', "#findHotel", function () {
+    citySearch($("#citySelector").val().trim());
+  });
+
+  function wifi() {
 
   }
 
-  function cost ()
-  {
+  function cost() {
 
   }
 
-  function pool ()
-  {
+  function pool() {
 
   }
 
-  function restaurant ()
-  {
-    
+  function restaurant() {
+
   }
 
   $(function () {
