e356ac3c7d409f11d61213648e78d1d107e252ba
jmona789 <jmona789@gmail.com>
2016-01-08 23:48:07 -0500

    added some more ways of writing cities to if statements to improve user experience

--- js/travel.js
+++ js/travel.js
@@ -2,22 +2,23 @@
 /*global $, jQuery, alert*/
 $(document).ready(function () {
 
+  "use strict";
   function citySearch(citySelector) {
-    if (citySelector === "New York") {
+    if (citySelector === "New York" || "NYC" || "New York City" || "New York, NY" || "New York City, NY") {
       $('#nyResults').show();
-    } else if (citySelector === "Chicago") {
+    } else if (citySelector === "Chicago" || "Chicago, IL") {
       $('#chicagoResults').show();
-    } else if (citySelector === "Miami") {
+    } else if (citySelector === "Miami" || "Miami, FL") {
       $('#miamiResults').show();
     } else if (citySelector === "Washington DC") {
       $('#dcResults').show();
-    } else if (citySelector === "Los Angeles") {
+    } else if (citySelector === "Los Angeles" || "Los Angeles, CA" ||"LA, CA" || "LA") {
       $('#losAngelesResults').show();
     }
   }
 
   $("body").on('click', "#findHotel", function () {
-    citySearch($("#citySelector").val().trim());
+    citySearch($("#citySelector").val().trim().toLowerCase());
   });
 
   function wifi() {
